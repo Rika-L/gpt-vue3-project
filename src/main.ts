c77cbf6338113768aa5b196a9ff7a6724ce8b796
Rika-L <2209393334@qq.com>
2024-02-24 10:04:37 +0800

    feat: 项目集成：elementPlus等

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+
+//引入elementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+import '@/style/index.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+
+//安装插件
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      viteMockServe({
+        localEnabled: command === "serve", //保证开发阶段可以使用mock接口
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
+      },
     },
-  },
-})
+    //scss全局变量的一个配置
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "@/style/variable.scss";',
+        },
+      },
+    },
+  };
+});
